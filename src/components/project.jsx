ac90529c2e1ab746c6c5b281afe8b1ebe15f51b5
John Caudill <jcaudill86@gmail.com>
2026-01-24 16:59:10 -0500

    final final push

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,13 +1,8 @@
-import './Project.css'
+import './project.css'
 
 function Project({ title, description, technologies, link, image }) {
   return (
     <div className="project-card">
-      {image && (
-        <div className="project-image">
-          <img src={image} alt={title} />
-        </div>
-      )}
       <div className="project-content">
         <h3 className="project-title">{title}</h3>
         <p className="project-description">{description}</p>
